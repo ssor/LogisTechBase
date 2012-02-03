85fdded20ba1ffa8453463004d8e83e4ccd03566
ssor <ssor@qq.com>
2012-02-03 15:12:00 +0800

    接口

---
+++ LogisTechBase/sysSetting/ISysSettingItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LogisTechBase
+{
+    public interface ISysSettingItem
+    {
+        //添加和清除控件
+        void addControls(Control.ControlCollection controlCollection);
+        void removeControls(Control.ControlCollection controlCollection);
+
+
+    }
+}
